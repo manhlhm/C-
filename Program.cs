3f85efb0052cf4b4c51f5f911078a1d683a23834
manhlhm <huynhmanhmirco@gmail.com>
2017-03-08 10:30:43 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lad8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bien1;
+            bien1 = 5;
+            Console.WriteLine(" gia tri bien 1 la {0}", bien1);
+            bien1 = 19;
+            Console.WriteLine(" gia tri khoi tao lan 2 {0}", bien1);
+            Console.ReadLine();
+            // cac bien cua C# co the dc khoi tao nhung khong co gia tri va co the gan gia tri sau.
+        }
+    }
+}
